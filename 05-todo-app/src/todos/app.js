9ed8082177634cedffff0fafbe44cfc4becc9338
Manuel Gordillo <manuelgordillodeveloper@gmail.com>
2025-12-22 16:13:54 -0500

    App todo hasta eliminar

---
+++ 05-todo-app/src/todos/app.js
@@ -0,0 +1,53 @@
+import todoStore from "../store/todo.store";
+import html from "./app.html?raw";
+import { renderTodos } from "./uses-cases";
+
+const ElementIds = {
+  TodoList: ".todo-list",
+  NewTodoInput: "#new-todo-input",
+};
+
+/**
+ * @param {String} elementId
+ */
+
+export const App = (elementId) => {
+  const displayTodos = () => {
+    const todos = todoStore.getTodos(todoStore.getCurrentFilter());
+    renderTodos(ElementIds.TodoList, todos);
+  };
+
+  (() => {
+    // Cuando la función App() se llama
+    const app = document.createElement("div");
+    app.innerHTML = html; //{{name}}
+    document.querySelector(elementId).append(app);
+    displayTodos();
+  })();
+
+  //Referencias HTML
+  const newDescriptionInput = document.querySelector(ElementIds.NewTodoInput);
+  const todoListUL = document.querySelector(ElementIds.TodoList);
+
+  //Listeners
+  newDescriptionInput.addEventListener("keyup", (event) => {
+    if (event.keyCode !== 13) return;
+    if (event.target.value.trim().length === 0) return;
+
+    todoStore.addTodo(event.target.value);
+    displayTodos();
+    event.target.value = " ";
+  });
+
+  todoListUL.addEventListener("click", (event) => {
+    const element = event.target.closest("[data-id]");
+    todoStore.toggleTodo(element.getAttribute("data-id"));
+    displayTodos();
+  });
+
+  todoListUL.addEventListener("click", (event) => {
+    const isDestroyElement = event.target.className === "destro";
+
+    const element = event.target.closest("[data-id]");
+  });
+};
