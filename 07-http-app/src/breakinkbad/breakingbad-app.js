6fd6522d26ddb8d82d0ac504b5a53aaf5e63d3cd
Manuel Gordillo <manuelgordillodeveloper@gmail.com>
2026-01-13 19:03:45 -0500

    update endpoint starter

--- 07-http-app/src/breakinkbad/breakingbad-app.js
+++ 07-http-app/src/breakinkbad/breakingbad-app.js
@@ -1,15 +1,41 @@
 /**
- * @returns {Object}
+ * @returns {Object} quote information
  */
 const fetchQuote = async () => {
   const res = await fetch("https://www.breakingbadapi.com/api/quote/random");
+  const data = await res.json();
+
+  return data;
 };
 
 /**
  *
  * @param {HTMLDivElement} element
  */
-export const BreakingbadApp = (element) => {
+export const BreakingbadApp = async (element) => {
   document.querySelector("#app-title").innerHTML = "Breakingbad App";
   element.innerHTML = "Loading...";
+
+  // const quote = await fetchQuote();
+  // element.innerHTML = "Tenemos Data...";
+
+  const quoteLabel = document.createElement("blockquote");
+  const authoLabel = document.createElement("h3");
+  const nextQuouteButton = document.createElement("button");
+  nextQuouteButton.innerText = "Next Quote";
+
+  //Añadir Listener
+  nextQuouteButton.addEventListener("click", async () => {
+    element.innerHTML = " Loading...";
+    const quote = await fetchQuote();
+    renderQuote(quote);
+  });
+
+  const renderQuote = (data) => {
+    quoteLabel.innerHTML = data.quote;
+    authoLabel.innerHTML = data.author;
+    element.replaceChildren(quoteLabel, authoLabel, nextQuouteButton);
+  };
+
+  fetchQuote().then(renderQuote);
 };
